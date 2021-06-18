f865163cc60e8457453b20280112e6c6a4a94c7e
democvidev <cnvvc_vlad@mail.ru>
2021-06-18 17:36:56 +0200

    updated files

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,19 +2,45 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Category;
+use App\Form\CategoryFormType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @Route("/admin", name="admin_")
+ */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/", name="home")
      */
-    public function index(): Response
+    public function home(): Response
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
+    }
+
+    /**
+     * @Route("/category/add", name="add_category")
+     */
+    public function addCategory(Request $request): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryFormType::class, $category);
+
+        $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($category);
+        $em->flush();
+        return $this->redirectToRoute('admin_home');
+    }
+    return $this->render('admin/category/add.html.twig', [
+        'form' => $form->createView(),
+    ]);
     }
 }
