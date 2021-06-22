940ba917d77db23188114b37f1d6ca6c5c237675
democvidev <cnvvc_vlad@mail.ru>
2021-06-22 12:13:36 +0200

    find all posts

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Form\PostFormType;
+use App\Repository\PostRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,10 +15,14 @@ class PostController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function home(): Response
+    public function home(PostRepository $postRepository): Response
     {
+        // 
+        $posts = $postRepository->findAll(); // injection de dépendances PostRepository dans la signature de la méthode
+        // dd($posts);
         return $this->render('post/home.html.twig', [
             'bg_image' => 'clean/assets/img/home-bg.jpg',
+            'posts' => $posts
         ]);
     }
 
